685636dfcd8a21de9ef1e90805fec691f3fc7cb7
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-05-13 14:53:21 +0300

    refactor the code

--- src/main/java/com/example/booksManager/BooksManagerApplication.java
+++ src/main/java/com/example/booksManager/BooksManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BooksManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BooksManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BooksManagerApplication.class, args);
+    }
 
 }
